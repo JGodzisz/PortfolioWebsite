48a8faa0a2008a1a6effbe36f762fe3fbce7f6f1
Jeffrey Godzisz <Jeffreygodzisz@gmail.com>
2023-05-14 08:39:55 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+//var person = prompt("Please enter your name", "Your Name Here"); document.getElementById("nameInput").innerHTML = "Hello, " + person + "! Welcome to my portfolio page.";
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
